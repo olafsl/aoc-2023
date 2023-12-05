7b18ee0fdd047bcda486745e01c8ca37cb9b3aaa
Olaf Slomp <git@oslomp.nl>
2023-12-05 20:31:45 +0100

    Duizend inefficiente 5b

--- aoc_5/src/main.rs
+++ aoc_5/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{prelude::*, self};
+use std::io::{self, prelude::*};
 use std::time::Instant;
 
 fn filename_to_string(s: &str) -> io::Result<String> {
@@ -12,37 +12,55 @@ fn filename_to_string(s: &str) -> io::Result<String> {
 fn string_to_ranges(string: &str) -> Vec<(isize, isize, isize)> {
     let mut ranges: Vec<(isize, isize, isize)> = Vec::new();
     for transform in string.lines().skip(1) {
-        let [destination, source, range] = transform.split_whitespace().map(|x| x.parse::<isize>().unwrap()).collect::<Vec<isize>>()[..] else {panic!()};
-        ranges.push((source, source+range, destination-source));
+        let [destination, source, range] = transform
+            .split_whitespace()
+            .map(|x| x.parse::<isize>().unwrap())
+            .collect::<Vec<isize>>()[..]
+        else {
+            panic!()
+        };
+        ranges.push((source, source + range, destination - source));
     }
     return ranges;
 }
 
-
 fn main() {
     let start = Instant::now();
-    let Ok(file) = filename_to_string("./src/input") else { panic!("Could not open File")};
+    let Ok(file) = filename_to_string("./src/input") else {
+        panic!("Could not open File")
+    };
     let chunks = file.split("\n\n").collect::<Vec<&str>>();
-    let seeds = chunks[0].split(" ").skip(1).map(|x| x.parse::<isize>().unwrap()).collect::<Vec<isize>>();
-    let transforms = chunks[1..].iter().map(|x| string_to_ranges(x)).collect::<Vec<Vec<(isize, isize, isize)>>>();
-    let mut lowest_loc= isize::MAX;
+    let seeds = chunks[0]
+        .split(" ")
+        .skip(1)
+        .map(|x| x.parse::<isize>().unwrap())
+        .collect::<Vec<isize>>();
+    let transforms = chunks[1..]
+        .iter()
+        .map(|x| string_to_ranges(x))
+        .collect::<Vec<Vec<(isize, isize, isize)>>>();
+    let mut lowest_loc = isize::MAX;
 
-    for seed in seeds {
-        let mut val = seed;
-        for maps in &transforms {
-            for map in maps {
-                if map.0 <= val && val < map.1 {
-                    val += map.2;
-                    break
+    let ranges = seeds.chunks(2).collect::<Vec<_>>();
+    for range in ranges {
+        for seed in range[0]..range[0] + range[1] {
+            let mut val = seed;
+            for maps in &transforms {
+                for map in maps {
+                    if map.0 <= val && val < map.1 {
+                        val += map.2;
+                        break;
+                    }
                 }
             }
-        }
-        if val <= lowest_loc {
-            lowest_loc = val;
+            if val <= lowest_loc {
+                println!("New low point: {val}");
+                lowest_loc = val;
+            }
         }
     }
+
     println!("{lowest_loc}");
     let duration = start.elapsed();
     println!("Time elapsed: {:?}", duration)
-
 }
