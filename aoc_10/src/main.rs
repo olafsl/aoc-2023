49d8a756d93f4be4adff222255e4d721273628c9
Olaf Slomp <git@oslomp.nl>
2023-12-11 01:21:52 +0100

    day 10 scaffold

---
+++ aoc_10/src/main.rs
@@ -0,0 +1,29 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::time::Instant;
+
+enum Direction {
+    Vertical,
+    Horizontal,
+    UpRight,
+    UpLeft,
+    DownRight,
+    DownLeft,
+}
+
+fn function(file: BufReader<File>) {
+    file.lines().map(|x| x.unwrap()).reduce(|x, a| (x + &a));
+
+
+}
+
+
+
+
+fn main() {
+    let start = Instant::now();
+    let contents = BufReader::new(File::open("./src/input").expect("Should have been able to read the file"));
+    function(contents);
+    let duration = start.elapsed();
+    println!("Time elapsed: {:?}", duration)
+}
